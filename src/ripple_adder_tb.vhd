84aea1d650689ad944c2ace7d6a46344afee2dff
diegoromerorojas <225315540+diegoromerorojas@users.noreply.github.com>
2026-02-10 13:56:09 -0700

    ripple adder TB runs successfully

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,14 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with Cin" severity failure;
+	   w_addends <= x"59"; w_Cin <= '0'; wait for 10ns;
+	       assert(w_sum = x"E" and w_Cout = '0') report "bad with line 151" severity failure;
+	   w_addends <= x"69"; w_Cin <= '0'; wait for 10ns;
+	       assert(w_sum = x"F" and w_Cout = '0') report "bad with line 152" severity failure;
+	   w_addends <= x"79"; w_Cin <= '0'; wait for 10ns;
+	       assert(w_sum = x"0" and w_Cout = '1') report "bad with line 153" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
